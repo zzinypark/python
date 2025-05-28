b22d0222eebcd68e8d4d059b7bebbb1d56deb6c8
zzinypark <zzinypark21@gamil.com>
2025-05-28 14:17:38 +0900

    선택과제

---
+++ 문제풀이용.py
@@ -0,0 +1,21 @@
+print(14%3)
+
+print(float(17//3))
+print(int(10)-9)
+print(float(10)-2)
+#주석
+
+print(10//3)
+print(int(5.7))
+print(float(3))
+print(7%2)
+
+print(3/2)
+
+print(float(7))      
+print(float("3.14"))  
+print(float(3 + 2))   
+
+print(divmod(17, 4))
+
+print(pow(2,3))
