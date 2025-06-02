1b7946a12dba2eddb90edc25246bc4c4d5a8169b
zzinypark <zzinypark21@gamil.com>
2025-06-02 17:54:29 +0900

    추가과제

--- 문제풀이용.py
+++ 문제풀이용.py
@@ -1,23 +1,5 @@
-이름 = "김오즈"
-나이 = 14
-소속 = "마법중"
-등급 = 3
+i = 0
+while i < 100:
+    i += 23
 
-print(type(이름))
-
-print(type(float(10)-2))
-print(type(3.0 + 7.0))
-
-과일 = 'apple'
-print(과일.count('p'))
-
-print(8 == 2 * 4,
-    4 != 2 + 2,
-    2 * 3 is 3 + 3,
-    8 is 4 * 2.0,
-    5 is 6 - 1.1)
-
-print('text' and True)
-
-x = int(input())
-print(x % sum(int(i) for i in str(x)) == 0)
+print(i)
