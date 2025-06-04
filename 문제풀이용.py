2f2334d3f0d7109aa2f0f6787c02bb94c939d47b
zzinypark <zzinypark21@gamil.com>
2025-06-04 15:39:33 +0900

    도전문법

--- 문제풀이용.py
+++ 문제풀이용.py
@@ -1,4 +1,10 @@
-i = 0
-while i < 100:
-    i += 23
-print(i)
+def 함수0(num):
+    return num * 2
+def 함수1(num):
+    return 함수0(num + 2)
+def 함수2(num):
+    num =  num + 10
+    return 함수1(num)
+
+c = 함수2(2)
+print(c)
